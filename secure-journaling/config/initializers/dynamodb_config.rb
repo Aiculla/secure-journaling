c01583322e089019a1322722635d5ddb39f60444
Aiculla <walidmuhammad1@gmail.com>
2023-05-19 09:16:29 -0400

    secrets

--- secure-journaling/config/initializers/dynamodb_config.rb
+++ secure-journaling/config/initializers/dynamodb_config.rb
@@ -2,5 +2,5 @@ require 'aws-sdk-dynamodb'
 
 Aws.config.update({
     region: 'us-east-1',
-    credentials: Aws::Credentials.new('', '')
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'],ENV['AWS_SECRET_KEY'])
   })
