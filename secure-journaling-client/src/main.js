91c06abc6375f6c9bedcbb28235b3d177bf0f09b
Aiculla <walidmuhammad1@gmail.com>
2023-05-23 08:44:18 -0400

    vue changes

--- secure-journaling-client/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-console.log(process.env.VUE_AWS_ACCESS_KEY);
-console.log(process.env.VUE_AWS_SECRET_KEY);
-createApp(App).use(router).mount('#app')
-
-
